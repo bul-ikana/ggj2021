90cedb155c914e51d407659191e78df4adfe49a3
Jesus Lopez <jesus.loma761@gmail.com>
2021-01-30 21:16:53 -0600

    Codigo innecesario y comentarios

--- Assets/TravelNode.cs
+++ Assets/TravelNode.cs
@@ -4,24 +4,18 @@ using UnityEngine;
 
 public class TravelNode : MonoBehaviour
 {
+    // Immediate nodes, added from inspector
     public List<GameObject> neighborNodes;
-
-    private List<Vector3> neighborNodesPositions;
 
     // Start is called before the first frame update
     void Start()
     {
-        neighborNodesPositions = neighborNodes.ConvertAll(new System.Converter<GameObject, Vector3>(toPosition));
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    private Vector3 toPosition(GameObject go)
-    {
-        return go.gameObject.transform.position;
     }
 }
